d2cbe2f8580a9ac954963a196941c7d1d789ccba
SHINAS <dexterdlotta@gmail.com>
2026-01-09 21:47:43 -0800

    Added new gallery images

--- pages/gallery.js
+++ pages/gallery.js
@@ -14,6 +14,34 @@ export default function Gallery() {
         <img src="/gallery/decor6.jpg" alt="desing6" />
         <img src="/gallery/decor7.jpg" alt="desing7" />
         <img src="/gallery/decor8.jpg" alt="desing8" />
+         <img src="/gallery/decor9.jpg" alt="desing9" />
+          <img src="/gallery/decor10.jpg" alt="desing10" />
+           <img src="/gallery/decor11.jpg" alt="desing11" />
+            <img src="/gallery/decor12.jpg" alt="desing12" />
+             <img src="/gallery/decor13.jpg" alt="desing13" />
+              <img src="/gallery/decor14.jpg" alt="desing14" />
+               <img src="/gallery/decor15.jpg" alt="desing15" />
+                <img src="/gallery/decor16.jpg" alt="desing16" />
+                 <img src="/gallery/decor17.jpg" alt="desing17" />
+                  <img src="/gallery/decor18.jpg" alt="desing18" />
+                   <img src="/gallery/decor19.jpg" alt="desing19" />
+                    <img src="/gallery/decor20.jpg" alt="desing20" />
+                     <img src="/gallery/decor21.jpg" alt="desing21" />
+                      <img src="/gallery/decor22.jpg" alt="desing22" />
+                       <img src="/gallery/decor23.jpg" alt="desing23" />
+                        <img src="/gallery/decor24.jpg" alt="desing24" />
+                         <img src="/gallery/decor25.jpg" alt="desing25" />
+                          <img src="/gallery/decor26.jpg" alt="desing26" />
+                           <img src="/gallery/decor27.jpg" alt="desing27" />
+                            <img src="/gallery/decor28.jpg" alt="desing28" />
+                             <img src="/gallery/decor29.jpg" alt="desing29" />
+                              <img src="/gallery/decor30.jpg" alt="desing30" />
+                               <img src="/gallery/decor31.jpg" alt="desing31" />
+                                <img src="/gallery/decor32.jpg" alt="desing32" />
+                                 <img src="/gallery/decor33.jpg" alt="desing33" />
+                                  <img src="/gallery/decor34.jpg" alt="desing34" />
+                                   <img src="/gallery/decor35.jpg" alt="desing35" />
+                                    <img src="/gallery/decor36.jpg" alt="desing36" />
          <h10>For more desings and details jest connect us.</h10>
          <WhatsAppButton />
                <CallButton />
